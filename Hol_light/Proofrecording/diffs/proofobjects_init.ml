07ea57aff1976a6fac2545ad6982b971232a1e46
Chantal Keller <chantal.keller@wanadoo.fr>
2010-03-11 17:23:08 +0100

    Version initiale de HOL-Light

---
+++ Hol_light/Proofrecording/diffs/proofobjects_init.ml
@@ -0,0 +1,23 @@
+let (use_proofobjects, use_extended_proofobjects, use_coq) =
+  try
+    let n = Sys.getenv "HOLPROOFOBJECTS" in
+    if n = "BASIC" then
+      (true, false, false)
+    else if n = "EXTENDED" then
+      (true, true, false)
+    else if n = "COQ" then
+      (true, false, true)
+      (* (true, true, true) *)
+    else
+      (false, false, false)
+  with Not_found -> (false, false, false);;
+
+let _ =
+  if use_proofobjects then
+    if use_coq then
+      loads "proofobjects_dedukti.ml"
+      (* loads "proofobjects_coq.ml" *)
+    else
+      loads "proofobjects_trt.ml"
+  else
+    loads "proofobjects_dummy.ml";;
