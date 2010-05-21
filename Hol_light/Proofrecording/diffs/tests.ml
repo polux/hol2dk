803d3fb2dd919a3a9e030834e9a65bb8113c7448
Chantal KELLER <keller@lix.polytechnique.fr>
2010-05-21 12:37:52 +0200

    Test des règles SYM et TRANS

--- Hol_light/Proofrecording/diffs/tests.ml
+++ Hol_light/Proofrecording/diffs/tests.ml
@@ -1,5 +1,24 @@
+(* Rule REFL *)
+
 let test_refl = REFL `x:bool`
 
 let _ = save_thm "test_refl" test_refl
 
-let _ = export_list ["test_refl"]
+
+(* Rule SYM *)
+
+let test_sym = SYM (REFL `x:bool`);;
+
+let _ = save_thm "test_sym" test_sym
+
+
+(* Rule TRANS *)
+
+let test_trans = TRANS (SYM (REFL `x:bool`)) (SYM (REFL `x:bool`));;
+
+let _ = save_thm "test_trans" test_trans
+
+
+(* Export *)
+
+let _ = export_list ["test_refl"; "test_sym"; "test_trans"]
